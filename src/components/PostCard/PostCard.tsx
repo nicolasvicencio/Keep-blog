29bb6f155aa9b8421ea17f364c544ccb23633782
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-06-23 23:26:39 -0400

    Changing main page

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -2,14 +2,14 @@ import parseDate from "@/scripts/parseDate";
 import { PostMetadata } from "@/types/types";
 import Link from "next/link";
 import React from "react";
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 type Props = {
   post: PostMetadata;
 };
 
 export default function PostCard({ post }: Props) {
   return (
-    <div className="p-8 card rounded-md">
+    <Link href={`posts/${post.slug}`} className="p-8  rounded-md">
       <img
         src={post.thumbnail}
         alt="thumbnail"
@@ -17,16 +17,14 @@ export default function PostCard({ post }: Props) {
       />
       <h3 className="pt-2 text-xl font-bold">{post.title}</h3>
       <p className="py-2">{post.subtitle}</p>
-      <div className="flex justify-between items-center">
-        <Link
-          href={`posts/${post.slug}`}
-          key={post.slug}
-          className="rounded-s btn btn-primary"
-        >
-          Read Article
-        </Link>
-        <span className="text-sm align-bottom">{parseDate(post.date)}</span>
+      <div className="flex justify-between items-center my-2">
+        <p className="flex items-center gap-1">
+          Read more <AiOutlineArrowRight />
+        </p>
+        <span className="text-sm align-bottom text-[#6BAFC6]">
+          {parseDate(post.date)}
+        </span>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/components/PostFeed/PostFeed.tsx
+++ src/components/PostFeed/PostFeed.tsx
@@ -2,17 +2,46 @@ import React from "react";
 import Link from "next/link.js";
 import { getPostMetadata } from "../../scripts/helperPost";
 import { PostCard } from "../PostCard";
+import parseDate from "@/scripts/parseDate";
+import { BiTimeFive } from "react-icons/bi";
 type Props = {};
 
 export default function PostFeed({}: Props) {
   const postMetadata = getPostMetadata();
 
   return (
-    <div className="md:w-[80%] mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto">
-        {postMetadata.map((post) => (
-          <PostCard key={post.slug} post={post} />
-        ))}
+    <div className="md:w-[90%] mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto">
+        {postMetadata.map((post, _i) => {
+          if (_i < 3) {
+            return <PostCard key={post.slug} post={post} />;
+          }
+        })}
+      </div>
+      <div className="my-10">
+        {postMetadata.map((post, _i) => {
+          if (_i >= 3) {
+            return (
+              <Link
+                href={`posts/${post.slug}`}
+                className="flex justify-between items-center border-y border-y-gray-900 p-4"
+              >
+                <div className="flex flex-col gap-1">
+                  <div className="flex gap-4 text-[#6BAFC6]">
+                    <BiTimeFive className="text-sm" />
+                    <span className="text-xs">{parseDate(post.date)}</span>
+                  </div>
+                  <h3 className="text-lg">{post.title}</h3>
+                </div>
+                <img
+                  src={post.thumbnail}
+                  alt="post thumbnail"
+                  className="h-36"
+                />
+              </Link>
+            );
+          }
+        })}
       </div>
     </div>
   );
